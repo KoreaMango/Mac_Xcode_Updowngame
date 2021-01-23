8935a5f8fbcf2e846d77356a7d08fb2f17b431a3
KoreaMango <mgo8434@naver.com>
2021-01-23 15:36:34 +0900

    UpdownGame First Commit_YaGom 1~4 Lecture

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLable: UILabel!
+    @IBOutlet weak var sliderValueLable: UILabel!
+    @IBOutlet weak var minimumValueLable: UILabel!
+    @IBOutlet weak var maximumValueLable: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        slider.minimumValue = 0.0
+        slider.maximumValue = 30.0
+        slider.value = 15.0
+        
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        reset()
     }
 
     @IBAction func sliderValueChanged(_ sender: UISlider){
@@ -26,6 +39,23 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton){
         print("Touch Reset Button")
+        reset()
+    }
+    
+    func reset(){
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLable.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLable.text = "0"
+        maximumValueLable.text = "30"
+        sliderValueLable.text = "15"
+        
+        
     }
     
     
